d074731a03bfaf7dc68f4ef3fa42d6a5d14b9aab
markwilloughby <mwilloughby1982@gmail.com>
2019-03-05 14:50:58 -0500

    commit

--- mark/newmacinstall.sh
+++ mark/newmacinstall.sh
@@ -48,23 +48,27 @@ sudo installer -pkg zoomusInstaller.pkg -target /
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install apps from homebrew #
+brew update
+brew upgrade
 brew install blueutil tmux watch tree jq
 brew install minio/stable/mc
 brew install cloudfoundry/tap/credhub-cli
 brew install cloudfoundry/tap/bosh-cli
 brew install cloudfoundry/tap/cf-cli
 brew install homebrew/cask/powershell
-<<<<<<< HEAD
 brew cask install iterm2
 brew install zsh
 brew install mas
+
+brew install python3
+brew install python-dev3 python-pip3 python-virtualenv libsasl2-dev python-dev libldap2-dev libssl-dev default-libmysqlclient-dev
+brew install dig
+brew xcode-select --install
+brew install direnv
+brew install terraform
+brew install wget aws-cli
+
 mas install 715768417 803453959 595191960 497799835 408981434 405399194
-=======
-#brew cask install iterm2
-##brew install zsh
-brew install mas
-mas install 715768417 803453959
->>>>>>> d84fc4fe47e886c8ccd18b97afaf0e5349c47e59
 gem install cf-uaac
 
 # install fly-cli #
@@ -83,16 +87,3 @@ unzip ~/Downloads/VSCode-darwin-stable.zip
 # install oh-my-zsh (drops you into zsh and you must exit)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-#om-darwin
-#cd ~/Downloads
-#om_type=darwin
-#curl -L --remote-name $(curl -s https://api.github.com/repos/pivotal-cf/om/releases/latest | jq -r ".assets[] | select(.name | test(\"${om_type}\")) | .browser_download_url")
-#chmod +x om-darwin
-#mv om-darwin /usr/local/bin/om
-
-# due to great reluctance, I'm allowing concourse to be installed #
-#cd ~/Downloads
-#concourse_type=concourse_darwin_amd64$
-#curl -L --remote-name $(curl -s https://api.github.com/repos/concourse/concourse/releases/latest | jq -r ".assets[] | select(.name | test(\"${concourse_type}\")) | .browser_download_url")
-#chmod +x concourse_darwin_amd64
-#mv concourse_darwin_amd64 /usr/local/bin/concourse
